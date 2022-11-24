76c668cc5b9c6de66fe4d7050deee361a0d5e57b
Harrison Thomas <harrison.thomas04@gmail.com>
2022-11-23 20:54:29 -0600

    removed boilerplate; added favicon

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  bracketSpacing: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  semi: false,
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  endOfLine: 'auto',
+}
